d02b5cf4fe9fe0fe6c30ff65ca69c215dbe16e6a
vinodkanduri123 <133983884+vinodkanduri123@users.noreply.github.com>
2023-06-16 06:38:01 +0530

    Update ass2.py

--- ass2.py
+++ ass2.py
@@ -1,4 +1,4 @@
-'''s = "aabbcdeaabbceder"
+s = "aabbcdeaabbceder"
 count = {}
 
 for letter in s:
@@ -8,20 +8,20 @@ for letter in s:
         else:
             count[letter] = 1
 
-print(count)'''
+print(count)
 
 
-'''
+
 d = {"don": 100, "sunny": 200, "sai": 1, "shannu": 20}
 total = 0
 
 for value in d.values():
     total += value
 
-print(total)'''
+print(total)
 
 
-'''
+
 d = {"don": 100, "sunny": 200, "sai": 1, "shannu": 20}
 total = sum(d.values())
-print(total)'''
+print(total)
